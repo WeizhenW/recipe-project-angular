b9c84c02cead1e1ab90004f62b5de92dfc48b87c
Weizhen Wang <wangwzhen@gmail.com>
2021-12-17 17:35:46 -0600

    build the html pages

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipesList: Recipe[] = [
+    new Recipe('Gateau Basque', 'Nice and yummy!', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS118QoFEfvkBQKqIxmQoDRrf1N6TU3_rRjQ&usqp=CAU'),
+    new Recipe('Souffle', 'Delicious Chocolate Souffles', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8JHE5JZzI-V3Z6nIrvuWdux0_HPV-k9sMJRnKHhOz4cFBzqdptZ5ScgSpPJ5BH-itehk&usqp=CAU'),
+    new Recipe('Mont Blanc', 'My favorite French Dessert', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3OZQULtZWIwQz6qB6Snlp25KXY3L0j2SyIQ&usqp=CAU')
+
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredientsList: Ingredient[] = [
+    new Ingredient('flour', 100),
+    new Ingredient('egg', 5),
+    new Ingredient('chocolate', 200)
+  ]
 
   constructor() { }
 
