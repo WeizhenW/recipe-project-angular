3616f2fcc5f18d5bfab55cd05de7b6f8f9be8a56
Weizhen Wang <wangwzhen@gmail.com>
2021-12-21 14:17:29 -0600

    Add simple navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-project-angular';
+  pageToShow = 'Recipe';
+
+  onNavigate(event: string) {
+    this.pageToShow = event;
+  }
 }
